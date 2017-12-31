246bfc65549b91f29a502f4b1dbae14b4f876689
mikasaaL <33093708+mikasaaL@users.noreply.github.com>
2017-12-31 12:44:05 -0800

    Add files via upload

--- src/JsonReceiver.py
+++ src/JsonReceiver.py
@@ -1,4 +1,4 @@
-import urllib2
+from urllib.request import urlopen
 
 
 class JsonReceiver:
@@ -9,11 +9,19 @@ class JsonReceiver:
         self.accountKey += key
 
     def get_json_summoner_info(self, account_id):
-        summoner_url = "/lol/summoner/v3/summoners/by-account/" + account_id
-        f = urllib2.urlopen(self.rootUrl + summoner_url + self.accountKey)
-        return f.read()
+        summoner_url = "/lol/summoner/v3/summoners/by-account/" + str(account_id)
+        full_url = urlopen(self.rootUrl + summoner_url + self.accountKey)
+        return full_url.read()
 
-
-jr = JsonReceiver("RGAPI-9e18de3a-cb58-4628-9465-42a2278808c7")
-res = jr.get_json_summoner_info("211349596")
-print(res)
+    def get_json_summoner_match(self,account_id):
+    	summoner_url = "/lol/match/v3/matchlists/by-account/" + str(account_id) + "/recent"
+    	full_url = urlopen(self.rootUrl + summoner_url + self.accountKey)
+    	return full_url.read()
+    def get_json_winning_info(self,summoner_id):
+    	summoner_url = "/lol/league/v3/positions/by-summoner/" + str(summoner_id)
+    	full_url = urlopen(self.rootUrl + summoner_url + self.accountKey)
+    	return full_url.read()
+    def get_json_game_info(self,game_Id):
+    	summoner_url = "/lol/match/v3/matches/" + str(game_Id)
+    	full_url = urlopen(self.rootUrl + summoner_url + self.accountKey)
+    	return full_url.read()
--- src/jsonToDict.py
+++ src/jsonToDict.py
@@ -1,7 +1,75 @@
+import json
+from JsonReceiver import JsonReceiver
+
+
+
 class JsonToDict:
-    JsonString = "blah"
+	jr = JsonReceiver("RGAPI-004a6fb4-cbd1-42dc-91e6-d5d40933108e")
 
-    def hello(self):
-        print("Hello There")
+	def getSummonerName(self,accountid):
+		input = self.bts(self.jr.get_json_summoner_info(accountid))
+		spdict = json.loads(input)
+		return str(spdict["name"])
 
-con = JsonToDict()
+	def getSummonerLevel(self,accountid):
+		input = self.bts(self.jr.get_json_summoner_info(accountid))
+		spdict = json.loads(input)
+		return str(spdict["summonerLevel"])
+
+	def getSummonerId(self,accountid):
+		input = self.bts(self.jr.get_json_summoner_info(accountid))
+		spdict = json.loads(input)
+		return(spdict["id"])
+
+	def getMatches(self,accountid):
+		input = self.bts(self.jr.get_json_summoner_match(accountid))
+		spdict = json.loads(input)
+		matches = list(spdict["matches"])
+		gameid_list = []
+		for match in matches:
+			gameid_list.append(match["gameId"])
+		return gameid_list
+
+
+	def winRates(self, summonerId):
+		input = self.bts(self.jr.get_json_winning_info(summonerId))
+		spdict = json.loads(input)
+		wr = spdict[0]["wins"]/(spdict[0]["wins"]+spdict[0]["losses"])
+		return wr
+
+	def playerRank(self,summonerId):
+		input = self.bts(self.jr.get_json_winning_info(summonerId))
+		spdict = json.loads(input)
+		rank = spdict[0]["tier"] + " " + spdict[0]["rank"]
+		return rank
+
+	def getParticipantId(self, gameId, accountid):
+		input = self.bts(self.jr.get_json_game_info(gameId))
+		spdict = json.loads(input)
+		playerList = spdict["participantIdentities"]
+		for player in playerList:
+			if(player["player"]["accountId"] == accountid):
+				partId = player["participantId"]
+		return partId
+
+	def playerKDA(self, partId,gameId):
+		input = self.bts(self.jr.get_json_game_info(gameId))
+		spdict = json.loads(input)
+		participants = spdict["participants"]
+		stats = participants[partId-1]
+
+		print(stats["stats"]["kills"], "/",stats["stats"]["deaths"], "/", stats["stats"]["assists"] )
+
+	def bts(self,input):
+		return str(input,'utf-8')
+
+name = JsonToDict()
+print("Summoner ID:", name.getSummonerName(211349596))
+level = JsonToDict()
+#print("Level:", level.getSummonerLevel("211349596"))
+num = JsonToDict()
+#print (num.getMatches("211349596"))
+
+print("%.2f"%(num.winRates(48423009)))
+print(num.playerRank(48423009))
+num.playerKDA(7, 2680404511)
